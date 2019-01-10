70ada256be59248296398dd8baff2cdfd801555a
Greyson Harris <gharris@esri.com>
2019-01-10 00:33:26 +0000

    Automatically backed up by Learn

--- winter.js
+++ winter.js
@@ -3,13 +3,14 @@ function fetchBooks() {
 var json = fetch("http://api.open-notify.org/astros.json")
 .then(response => response.json())
 .then(json => console.log(json));
+return json;
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(people => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${people.name}</h2>`
+    h2.innerHTML = `<h2>${people}</h2>`
     main.appendChild(h2)
   })
 }
